88ddc89dbb45385b4b49c573041c0185400b34fd
Todd Whitelow <todd@whitelow.tech>
2020-12-22 00:39:42 -0700

    add reviews-queries

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const reviewsDb = require('./reviews-queries');
+
 
 app.use(bodyParser.json());
 app.use(
@@ -13,6 +15,13 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
 });
+
+app.get('/reviews', reviewsDb.getReviews);
+app.get('/reviews/:id', reviewsDb.getReviewById);
+app.post('/reviews', reviewsDb.createReview);
+app.put('/reviews/:id', reviewsDb.updateReview);
+app.delete('/reviews/:id', reviewsDb.deleteReview);
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
